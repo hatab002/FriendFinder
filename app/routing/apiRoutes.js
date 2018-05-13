95c5cfe3dc6fbb13aee69ceaaec178576bd8e58d
hatab002 <hatab002@umn.edu>
2018-05-12 23:15:18 -0500

    working on apiRoutes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,16 +1,35 @@
-var friendList = require ("../data/friends")
+var friendList = require("../data/friends")
 
+module.exports = function (app) {
 
-module.exports = function (app){
+    app.get("/api/friends", (req, res) => {
+        res.json(friendList);
+    });
 
+    app.post("/api/friends", (req, res) => {
+        var chosenMatch = {
+            name: "",
+            photo: "",
+            difference: 2000
+        };
+        var userInfo = req.body;
+        var userScores = userInfo.scores;
+        var friendDifference = 0;
 
-app.get("/api/friends", (req, res) => {
-    res.json(friendList);
-});
+        for (let i = 0; i < friendList.length; i++) {
+            friendDifference = 0;
+            for (let j = 0; j < friendList[i].scores[j]; j++) {
+                friendDifference += Math.abs(parseInt(userScores[j]) - parseInt(friendList[i].scores[j]));
 
-
-app.post("/api/friends", (req, res) => {
-// friend comparison here
-});
+                if (friendDifference <= chosenMatch.difference) {
+                    chosenMatch.name = friendList[i].name,
+                        chosenMatch.photo = friendList[i].photo,
+                        chosenMatch.difference = friendDifference
+                }
+            }
+        }
+        friendList.push(userInfo);
+        res.json(chosenMatch);
+    });
 
 };
