4b29d35b4d44794ae4f38fe0e3fa6183e5089d6a
hatab002 <hatab002@umn.edu>
2018-05-13 21:59:31 -0500

    Finished

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,17 @@
 var friendList = [{
-    name: "",
-    photo: "",
-    scores: []
+    name: "Amy",
+    photo: "https://cdn.shopify.com/s/files/1/1102/4268/products/CrossFit_StickFigureFamily_Daughter_Deadlift_RFA.jpg?v=1475862980",
+    scores: [1, 3, 5, 2, 5, 4, 3, 1, 1, 3]
+},
+{
+    name: "John",
+    photo: "https://d30y9cdsu7xlg0.cloudfront.net/png/1285891-200.png",
+    scores: [3, 3, 2, 3, 1, 4, 5, 1, 2, 3]
+},
+{
+    name: "Ralph",
+    photo: "https://i.pinimg.com/originals/69/0d/a5/690da5ba95504d6e19ea02bfdb241c1d.jpg",
+    scores: [5, 5, 3, 2, 4, 5, 1, 2, 3, 5]
 }
 ];
 
