272adb15a5a64c270aa821cbf2f0701e594ca6d7
hatab002 <hatab002@umn.edu>
2018-07-29 23:18:42 -0500

    fixing route bug

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -10,7 +10,7 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/home.html"));
 });
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
-})
+// app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../public/home.html"));
+// })
 };
